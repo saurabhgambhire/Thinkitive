5a14c39439c5d200ab213224fb24284c9eb46cc7
saurabhgambhire <saurabhgambhire57@gmail.com>
2023-12-22 16:07:34 +0530

    .

--- OOPS/src/com/thinkitive/inheritance/singleLevel/Driver.java
+++ OOPS/src/com/thinkitive/inheritance/singleLevel/Driver.java
@@ -3,6 +3,7 @@ package com.thinkitive.inheritance.singleLevel;
 public class Driver {
     public static void main(String[] args) {
         Son son = new Son();
+        // single level Inheritance
         son.business();
     }
 }
