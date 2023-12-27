44c9bfb112e88477b110c1e8540abdb24400bda4
saurabhgambhire <saurabhgambhire57@gmail.com>
2023-12-27 17:15:17 +0530

    ,

--- DataStructures/src/com/thinkitive/datastructure/Array/ZeroOnecs.java
+++ DataStructures/src/com/thinkitive/datastructure/Array/ZeroOnecs.java
@@ -2,30 +2,33 @@ package com.thinkitive.datastructure.Array;
 
 public class ZeroOnecs {
     public static void main(String[] args) {
-        int[] arr = {1, 1, 1, 1, 0, 1, 1};
+        int[] arr = {1, 1, 1, 1, 1, 1, 1};
+        segregateZerosOnes(arr);
 
+    }
+
+    public static void segregateZerosOnes(int[] arr) {
         int st = 0;
         int end = arr.length - 1;
-        while (st < end) {
+
+        while (st <= end) {
             if (arr[st] == 0 && arr[end] == 1) {
                 st++;
                 end--;
-            } else if (arr[st] == 1 && arr[end] == 0) {
-                int temp = arr[st];
-                arr[st] = arr[end];
-                arr[end] = temp;
+            } else {
+                if (arr[st] == 1) {
+                    swap(arr, st, end);
+                }
                 st++;
-                end--;
-            }else if (arr[st] == 0 && arr[end] == 0){
-                st++;
-            } else if (arr[st] == 1 && arr[end] == 1) {
                 end--;
             }
-
         }
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
-        }
-
     }
+
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
 }
