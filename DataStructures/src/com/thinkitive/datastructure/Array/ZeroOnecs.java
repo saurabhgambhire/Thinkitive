832e80db7e886e5c947e200df2390eb826748338
saurabhgambhire <saurabhgambhire57@gmail.com>
2023-12-27 18:13:47 +0530

    .

--- DataStructures/src/com/thinkitive/datastructure/Array/ZeroOnecs.java
+++ DataStructures/src/com/thinkitive/datastructure/Array/ZeroOnecs.java
@@ -2,7 +2,7 @@ package com.thinkitive.datastructure.Array;
 
 public class ZeroOnecs {
     public static void main(String[] args) {
-        int[] arr = {1, 1, 1, 1, 1, 1, 1};
+        int[] arr = {1, 1, 0,0, 1, 1, 1};
         segregateZerosOnes(arr);
 
     }
